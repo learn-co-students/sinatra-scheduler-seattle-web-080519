a68d5f5ec067ec5a5c9453dadf7d623b36256126
github username <gregm9999@gmail.com>
2019-11-13 22:40:20 -0800

    Done.

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,27 @@
+class SchedulesController < ApplicationController
+
+  set :method_override, true
+
+  get '/schedule' do
+    @courses = Course.where(on_schedule: true)
+    @total_credits = @courses.inject(0) {|sum, course| sum + course.credits}
+    erb :'/schedule/index'
+  end
+
+  post '/schedule/:id' do
+    @course = Course.find(params[:id])
+    @courses = Course.where(on_schedule: true)
+    @total_credits = @courses.inject(0) {|sum, course| sum + course.credits}
+    if @total_credits + @course.credits <= 15
+      @course.update(on_schedule: true)
+    end
+    redirect to '/schedule'
+  end
+  
+  delete '/schedule/:id/delete' do
+    @course = Course.find(params[:id])
+    @course.update(on_schedule: false)
+    redirect to '/schedule'
+  end
+
+end
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
 require_relative './config/environment'
 
+use CoursesController
+use SchedulesController
 run ApplicationController
